357ed3a7a789e6464fcb060d676efb291b5d922c
Bia-source <bia_ferreirads@yahoo.com>
2024-03-07 12:18:26 -0300

    adicionado service de recipes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import * as ingredients from "./ingredients.service";
+import * as recipes from "./recipes.service";
 
 export const services = {
     ingredients,
+    recipes,
 
     storage: {
         imagePath: "https://jenszhyobsfpkepfmgtr.supabase.co/storage/v1/object/public/ingredients"
